14242670c6e68a6f359622cffea2536a87020499
eptate01 <ethanptate@gmail.com>
2024-04-13 00:55:32 -0400

    Finished Neural Network

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,39 +3,42 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-x = data
-y = data
+dataset = np.loadtxt('datasets/metformin.csv', delimiter=',',skiprows=1)
+data = dataset[:,1:]
+result = dataset[:,0]
 
+dataTensor = torch.tensor(data, dtype=torch.float32)
+resultTensor = torch.tensor(result, dtype=torch.float32).reshape(-1, 1)
 class PimaClassifier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.hidden1 = nn.Linear(4, 12)
+        self.hidden1 = nn.Linear(5, 12)
         self.act1 = nn.ReLU()
         self.hidden2 = nn.Linear(12, 8)
         self.act2 = nn.ReLU()
         self.output = nn.Linear(8, 1)
         self.act_output = nn.Sigmoid()
 
-    def forward(self, x):
-        x = self.act1(self.hidden1(x))
-        x = self.act2(self.hidden2(x))
-        x = self.act_output(self.output(x))
-        return x
+    def forward(self, data):
+        data = self.act1(self.hidden1(data))
+        data = self.act2(self.hidden2(data))
+        data = self.act_output(self.output(data))
+        return data
 
 model = PimaClassifier()
 print(model)
 
-loss_fn = nn.BCELoss()  # binary cross entropy
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+loss_fn = nn.L1Loss()  # binary cross entropy
+optimizer = optim.Adam(model.parameters(), lr=0.00001)
 
-n_epochs = 100
+n_epochs = 10
 batch_size = 10
 
 for epoch in range(n_epochs):
-    for i in range(0, len(X), batch_size):
-        Xbatch = X[i:i+batch_size]
+    for i in range(0, len(dataTensor), batch_size):
+        Xbatch = dataTensor[i:i+batch_size]
         y_pred = model(Xbatch)
-        ybatch = y[i:i+batch_size]
+        ybatch = resultTensor[i:i+batch_size]
         loss = loss_fn(y_pred, ybatch)
         optimizer.zero_grad()
         loss.backward()
@@ -45,7 +48,8 @@ for epoch in range(n_epochs):
 
 # compute accuracy (no_grad is optional)
 with torch.no_grad():
-    y_pred = model(X)
+    y_pred = model(dataTensor)
 
-accuracy = (y_pred.round() == y).float().mean()
+accuracy = (y_pred.round() == resultTensor).float().mean()
 print(f"Accuracy {accuracy}")
+torch.save(model.state_dict(), "./model")
